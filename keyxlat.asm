4264e73dab2fd2986edbfb55db98cd1facc5d803
Hamish Coleman <hamish@zot.org>
2020-06-12 14:03:45 +0100

    Translate the simple cursor diamond

--- keyxlat.asm
+++ keyxlat.asm
@@ -8,6 +8,11 @@ CH_BS   EQU 0x08
 CH_ESC  EQU 0x1b
 CH_DEL  EQU 0x7f
 
+WS_UP   EQU 'E'-'@'
+WS_DN   EQU 'X'-'@'
+WS_LT   EQU 'S'-'@'
+WS_RT   EQU 'D'-'@'
+
 ; patch the routine
     ORG UCONI
     jp MORPAT
@@ -28,5 +33,25 @@ CH_DEL  EQU 0x7f
     ret
 
 1:
+    call WSCONI
+    cp '['
+    ret nz      ; not a CSI, eat the escape and hand the rest back
+
+    call WSCONI
+
+    ; is this a simple cursor move?
+    cp 'A'
+    ret c
+    cp 'E'
+    ret nc
+
+    ld b,0
+    ld c,a
+    ld hl, to1-'A'
+    add hl,bc
+    ld a, (hl)
     ret
-    ; more conversions to be added here
+
+    ; the simple cursor diamond is ^[[A through ^[[D
+to1:
+    db  WS_UP, WS_DN, WS_RT, WS_LT
