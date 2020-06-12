b07fc1fa1ceeca1bc716e3188b3e299627356fd1
Hamish Coleman <hamish@zot.org>
2020-06-12 14:51:09 +0100

    Add home and end

--- keyxlat.asm
+++ keyxlat.asm
@@ -8,17 +8,25 @@ CH_BS   EQU 0x08
 CH_ESC  EQU 0x1b
 CH_DEL  EQU 0x7f
 
-WS_UP   EQU 'E'-'@'
-WS_DN   EQU 'X'-'@'
-WS_LT   EQU 'S'-'@'
-WS_RT   EQU 'D'-'@'
-
 ; patch the routine
     ORG UCONI
-    jp MORPAT
+    jp patch
 
 ; our patch function
     ORG MORPAT
+nextch:
+    db  0
+patch:
+    ; do we have a saved char?
+    ld hl, nextch
+    ld A, (hl)
+    cp 0
+    jr z, 1f
+    ld B, 0
+    ld (hl), B
+    ret
+
+1:
     call WSCONI
     cp CH_ESC
     jr z, 1f
@@ -42,16 +50,40 @@ WS_RT   EQU 'D'-'@'
     ; is this a simple cursor move?
     cp 'A'
     ret c
-    cp 'E'
+    cp 'I'
     ret nc
 
     ld b,0
     ld c,a
-    ld hl, to1-'A'
-    add hl,bc
-    ld a, (hl)
+    ld ix, to1-'A'
+    add ix,bc
+    cp 'E'
+    jr c, 1f
+
+CTRL_Q  EQU 'Q'-'@'
+
+    ; needs a ^Q prefix
+    ld a, (ix)
+    ld (hl), a
+    ld a, CTRL_Q
     ret
 
+    ; simple one char xlat
+1:
+    ld a, (ix)
+    ret
+
+
+WS_UP   EQU 'E'-'@'
+WS_DN   EQU 'X'-'@'
+WS_LT   EQU 'S'-'@'
+WS_RT   EQU 'D'-'@'
+
+WS_HOME EQU 's'
+WS_END  EQU 'd'
+
     ; the simple cursor diamond is ^[[A through ^[[D
+    ; home and end are ^[[H and ^[[F
 to1:
-    db  WS_UP, WS_DN, WS_RT, WS_LT
+    ;   A      B      C      D           F            H
+    db  WS_UP, WS_DN, WS_RT, WS_LT, 'E', WS_END, 'G', WS_HOME
